449a5a1c7006576082327bd6585b641ef312a66f
Jecky <agentstocks88@gmail.com>
2025-10-18 21:15:09 +0700

    perulangan

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,14 @@
+'''
+program perulangan membaca buku
+'''
+
+jumlah_buku = 10
+print('Mom said, "read all the books"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+    print(f'Buku ke {jumlah_buku_yang_sudah_dibaca}')
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
